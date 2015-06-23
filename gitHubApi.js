ccd9615330fae92d6230e33d2e92dd46a8acb202
joshpitzalis <joshpitzalis@gmai.com>
2015-06-23 16:16:56 +0100

    merging auth and api work | almost working, not yet though

--- gitHubApi.js
+++ gitHubApi.js
@@ -1,5 +1,3 @@
-// IMPORTANT: run 'npm install github' or none of this will work
-
 var GitHubApi = require("github");
 
 var github = new GitHubApi({
@@ -12,27 +10,33 @@ var github = new GitHubApi({
     // pathPrefix: "/api/v3", // for some GHEs; none for GitHub
     timeout: 5000,
     headers: {
-        "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+        "user-agent": "mvp-funnel-App" // GitHub is happy with a unique user agent
     }
 });
 
 github.authenticate({
     type: "oauth",
-    key: "clientID",
-    secret: "b22fe8aac0b12cdc53a7e54dc81f6c7b5f0685e1"
-})
+    token: "8a1d08c6126201302142c39078ab7d47c8eab2c1"
+    // "d236c7523ec4b5b91b4689dfaf619a50206a9672"
+});
 
-github.repos.getContent({
+github.authenticate({
+    type: "oauth",
+    key: "639ce8f90b60a61055ac",
+    secret: "b22fe8aac0b12cdc53a7e54dc81f6c7b5f0685e1"
+});
+
+github.repos.createFile({
 
     // headers: Optional. Key/ value pair of request headers to pass along with the HTTP request.
     // Valid headers are: 'If-Modified-Since', 'If-None-Match', 'Cookie', 'User-Agent', 'Accept', 'X-GitHub-OTP'.
     // id: 37860032,
     user : "jmnr",
     repo : "mvp-funnel",
-    // content: "monkey",
-    // message: "remote content write test",
-    path : "README.md",
-    ref : "github"
+    content: "bW9ua2V5",
+    message: "remote content write test",
+    path : "test.md",
+    branch : "github",
 }, function(err, res) {
     console.log(res);
 });
